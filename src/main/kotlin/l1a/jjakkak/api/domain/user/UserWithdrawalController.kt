73a51d13c30a93cea36be05d6f82973d6af79cfc
LeeDoyun <choiyr6476@naver.com>
2024-04-24 16:45:08 +0900

    token header에서 꺼내오는것으로 변경

--- src/main/kotlin/l1a/jjakkak/api/domain/user/UserWithdrawalController.kt
+++ src/main/kotlin/l1a/jjakkak/api/domain/user/UserWithdrawalController.kt
@@ -3,10 +3,13 @@ package l1a.jjakkak.api.domain.user
 import l1a.jjakkak.api.ApiUrl
 import l1a.jjakkak.api.domain.user.reqeust.UserWithdrawalRequest
 import l1a.jjakkak.api.domain.user.response.WithdrawalResponse
+import l1a.jjakkak.core.domain.user.UserId
 import l1a.jjakkak.core.domain.user.usecase.WithdrawalUseCase
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 )
 internal interface UserWithdrawalController {
     @PostMapping(ApiUrl.USER_WITHDRAWAL)
-    fun userWithdrawal(@RequestBody request: UserWithdrawalRequest): WithdrawalResponse
+    fun userWithdrawal(@RequestHeader headers: HttpHeaders): WithdrawalResponse
 
 
 
@@ -26,7 +29,9 @@ internal interface UserWithdrawalController {
 internal class UserWithdrawalControllerImpl (
     val useCase: WithdrawalUseCase
 ): UserWithdrawalController  {
-    override fun userWithdrawal(request: UserWithdrawalRequest): WithdrawalResponse =
-        useCase.withdrawal(request.toMessage()).run { WithdrawalResponse.from(this) }
 
+    override fun userWithdrawal(h: HttpHeaders): WithdrawalResponse {
+        var token = h.get("Authorization").toString().replace("Bearer ", "")
+        return  useCase.withdrawal(UserWithdrawalRequest(token).toMessage()).run { WithdrawalResponse.from(this) }
+    }
 }
