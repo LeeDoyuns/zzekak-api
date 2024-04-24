b480dcc4d6f25a6012526e8f9e1e2ee18ed5de76
LeeDoyun <choiyr6476@naver.com>
2024-04-24 16:53:59 +0900

    post->put Mapping으로 변경

--- src/main/kotlin/l1a/jjakkak/api/domain/user/UserWithdrawalController.kt
+++ src/main/kotlin/l1a/jjakkak/api/domain/user/UserWithdrawalController.kt
@@ -3,12 +3,10 @@ package l1a.jjakkak.api.domain.user
 import l1a.jjakkak.api.ApiUrl
 import l1a.jjakkak.api.domain.user.reqeust.UserWithdrawalRequest
 import l1a.jjakkak.api.domain.user.response.WithdrawalResponse
-import l1a.jjakkak.core.domain.user.UserId
 import l1a.jjakkak.core.domain.user.usecase.WithdrawalUseCase
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -19,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController
     consumes = [MediaType.APPLICATION_JSON_VALUE]
 )
 internal interface UserWithdrawalController {
-    @PostMapping(ApiUrl.USER_WITHDRAWAL)
+    @PutMapping(ApiUrl.USER_WITHDRAWAL)
     fun userWithdrawal(@RequestHeader headers: HttpHeaders): WithdrawalResponse
 
 
