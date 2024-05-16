c03fb988bcb86e5e6fb764d989aefc9c5e725279
LeeDoyuns <monaco5278@naver.com>
2024-05-16 19:57:59 +0900

    ktlint 처리

--- src/main/kotlin/l1a/jjakkak/api/config/security/WebConfig.kt
+++ src/main/kotlin/l1a/jjakkak/api/config/security/WebConfig.kt
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
-/**CorsFilter 대응*/
-
 @Configuration
 internal class WebConfig : WebMvcConfigurer {
-
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:8870", "https://zzekak.co.kr")
