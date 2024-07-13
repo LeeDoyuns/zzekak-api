ad9518bbc17d67e1eacbdffebbab3438785e2e73
Leedoyun <choiyr6476@naver.com>
2024-07-13 19:03:26 +0900

    Exception Enum 클래스 공백 제거

--- api/src/main/kotlin/com/zzekak/config/exception/ExceptionEnumDto.kt
+++ api/src/main/kotlin/com/zzekak/config/exception/ExceptionEnumDto.kt
@@ -10,9 +10,9 @@ enum class ExceptionEnumDto(
     val message: String
 ) {
     // 유저 관련 Erorr => U-...
-    ILLEGAL_TOKEN(" U-001", HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    ILLEGAL_TOKEN("U-001", HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
 
-    NO_EXIST_USER("U-002", HttpStatus.UNAUTHORIZED, "존재하지 않는 사용자 입니다."),
+    NO_EXIST_USER("U-002", HttpStatus.BAD_REQUEST, "존재하지 않는 사용자 입니다."),
 
     // server관련 Erorr => S-...
     SERVER_ERROR("S-001", HttpStatus.INTERNAL_SERVER_ERROR, "처리 중 오류가 발생했습니다.");
